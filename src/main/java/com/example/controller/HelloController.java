26b4570bfb3b3b165bdc01c31b4468bcd5beb2d9
OneCoderW <504474011@qq.com>
2019-05-22 17:39:12 +0800

    controller修改

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.controller;
 
 
+import com.example.dto.User;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +24,20 @@ public class HelloController {
         ServiceInstance instance = discoveryClient.getLocalServiceInstance();
         logger.info("/hello, host:" + instance.getHost()+",service_id:"+instance.getServiceId());
         return "Hello Word";
+    }
+
+    @RequestMapping(value = "/hellol", method= RequestMethod.GET)
+    public String hello(@RequestParam("name") String name) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "/hello2", method= RequestMethod.GET)
+    public User hello(@RequestHeader String name, @RequestHeader Integer age) {
+        return new User(name,age);
+    }
+    @RequestMapping(value = "/hello3", method= RequestMethod.POST)
+    public String hello(@RequestBody User user) {
+        return "Hello "+ user.getName() + ", " + user.getAge ();
     }
 
 
